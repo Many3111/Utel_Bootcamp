3c3091a116c9a1240ca8748dcd21e47cc330a355
Manuel Morales <mor4less05@gmail.com>
2023-10-04 01:13:36 -0600

    almost there

--- IMC_Utel.py
+++ IMC_Utel.py
@@ -1,4 +1,4 @@
-# como calcular IMC
+# Como calcular IMC
 
 def imc(estatura, peso):
     return peso / estatura**2
@@ -9,7 +9,36 @@ estatura = float(input('Escriba su estatura (m): '))
 
 Indice = imc(estatura, peso)
 
-
-print('Su IMC es: {}'.format(Indice))
+if Indice <= 1 and Indice >= 0:
+    print('Hubo un error en los datos ingresados favor de reingresar los datos en base a kg(peso) y m(altura)')
+elif Indice <= 24.9 and Indice >= 18.5:
+    print('Peso (Kg): {}'.format(peso))
+    print('Altura (m): {}'.format(estatura))
+    print('Su IMC se encuentra normal')
+elif Indice <= 29.9 and Indice >= 25:
+    print('Peso (Kg): {}'.format(peso))
+    print('Altura (m): {}'.format(estatura))
+    print('Su IMC es: {}'.format(Indice))
+    print('Su IMC indica que tiene ligero sobrepeso')
+elif Indice <= 34.9 and Indice >= 30:
+    print('Peso (Kg): {}'.format(peso))
+    print('Altura (m): {}'.format(estatura))
+    print('Su IMC es: {}'.format(Indice))
+    print('Su IMC indica que sufre de obesidad grado 1')
+elif Indice >= 40:
+    print('Peso (Kg): {}'.format(peso))
+    print('Altura (m): {}'.format(estatura))
+    print('Su IMC es: {}'.format(Indice))
+    print('Su IMC indica que sufre de obesidad grado 1')
+elif Indice <=24.8 and Indice >=2:
+    print('Peso (Kg): {}'.format(peso))
+    print('Altura (m): {}'.format(estatura))
+    print('Su IMC es: {}'.format(Indice))
+    print('La cantidad indicada de su IMC esta por debajo de los niveles recommendados, se recomienda consultar a su medico')
+else:
+    print('Peso (Kg): {}'.format(peso))
+    print('Altura (m): {}'.format(estatura))
+    print('Su IMC es: {}'.format(Indice))
+    print('Por favor de usar unicamente digitos para llenar su solicitud')
 
 
