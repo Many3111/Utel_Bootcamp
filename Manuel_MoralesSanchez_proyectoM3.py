08398911510848d4f4066a4e3e5c197629f6fb6a
Manuel Morales <mor4less05@gmail.com>
2023-12-06 00:12:16 -0600

    M3 final changes

---
+++ Manuel_MoralesSanchez_proyectoM3.py
@@ -0,0 +1,35 @@
+
+# - Agregue el import random para que me obtener resultados aleatorios
+# - Agregue el import matplotlib.pyolot para obtener una representacion 
+# grafica de los resultados
+
+import random
+import matplotlib.pyplot as plt
+
+def simular_canicas(num_canicas, niveles):
+    contenedores = [0] * (niveles + 1)  # Permite iniciar con 0 canicas en cada contenedor
+    
+    for i in range(num_canicas):
+        nivel_actual = 0
+        for i in range(niveles):
+            direccion = random.choice(["izquierda", "derecha"])
+            if direccion == "izquierda":
+                nivel_actual += 1
+        contenedores[nivel_actual] += 1  # Agregar la canica al contenedor correspondiente
+    
+    return contenedores
+
+def graficar_histograma(contenedores):
+    plt.bar(range(len(contenedores)), contenedores)
+    plt.xlabel('Contenedores')
+    plt.ylabel('Cantidad de canicas')
+    plt.title('Distribución de canicas en contenedores')
+    plt.show()
+
+# Parámetros de la simulación
+num_canicas = 3000
+niveles_obstaculos = 12
+
+# Simulación de las canicas y graficación del histograma
+resultados_canicas = simular_canicas(num_canicas, niveles_obstaculos)
+graficar_histograma(resultados_canicas)
